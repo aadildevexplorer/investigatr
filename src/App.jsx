2a425efb1c7f808a091b848fff537c12994a4a05
AadilKhan0461 <aadilkhan04610461@gmail.com>
2025-08-25 13:35:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Dashboard from "./Components/Dashboard";
+
+const App = () => {
+  return (
+
+    <>
+    <Dashboard /> 
+     {/* // dashboard */}
+     </>
+  )
+}
+
+export default App
